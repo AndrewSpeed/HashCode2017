7a74aa1610e968f91ec32d72873076ebcf8b7b56
Kris Watson <kris.watson@nanthealth.com>
2017-02-23 18:58:06 +0000

    Endpoint

--- src/main/java/EndpointDetails.java
+++ src/main/java/EndpointDetails.java
@@ -1,4 +1,21 @@
 
+
+import java.util.Map;
+
 public class EndpointDetails {
+	private int latency;
+	private Map<String, String> cacheServerMap;
 	
+	public int getLatency() {
+		return latency;
+	}
+	public void setLatency(int latency) {
+		this.latency = latency;
+	}
+	public Map<String, String> getCacheServerMap() {
+		return cacheServerMap;
+	}
+	public void setCacheServerMap(Map<String, String> cacheServerMap) {
+		this.cacheServerMap = cacheServerMap;
+	}
 }
