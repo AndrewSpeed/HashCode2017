4527fe939a94af9ae814238ce24a84ead1bf146b
Andrew Speed <andrew.cspeed@gmail.com>
2017-02-23 19:20:35 +0000

    Add parse logic for scenario details and videos

--- src/main/java/models/Video.java
+++ src/main/java/models/Video.java
@@ -3,6 +3,11 @@ package models;
 public class Video {
 	private int id;
 	private int size;
+
+	public Video(int id, int size) {
+	    this.id = id;
+	    this.size = size;
+    }
 	
 	public int getId() {
 		return id;
--- src/main/java/models/parser/InputParser.java
+++ src/main/java/models/parser/InputParser.java
@@ -1,5 +1,7 @@
 package models.parser;
 
+import models.Video;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -7,8 +9,16 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputParser {
-    File file;
-    Scanner scanner;
+    private File file;
+    private Scanner scanner;
+
+    private int videoCount;
+    private int endpointCount;
+    private int requestDescriptionCount;
+    private int cacheServerCount;
+    private int cacheServerCapacity;
+
+    private List<Video> videos;
 
     public InputParser(String fileName) {
         ClassLoader classLoader = this.getClass().getClassLoader();
@@ -24,9 +34,30 @@ public class InputParser {
     public void parse() {
         //File is found
         System.out.println("File Found : " + file.exists());
+        parseScenarioDetails(parseLineDetails(scanner.nextLine()));
+        videos = parseVideos(parseLineDetails(scanner.nextLine()));
+
         while(scanner.hasNextLine()) {
             System.out.println(scanner.nextLine());
         }
+    }
+
+    private List<Video> parseVideos(final List<Integer> integers) {
+        // line 2: size of each video in MB
+        List<Video> videos = new ArrayList<Video>();
+        for(int i=0; i < integers.size() - 1; i++) {
+            videos.add(new Video(i, integers.get(i)));
+        }
+        return videos;
+    }
+
+    private void parseScenarioDetails(final List<Integer> integers) {
+        // line 1: number of videos, number of endpoints, number of request descriptions, number of cache servers, cache server capacity (MB)
+        this.videoCount = integers.get(0);
+        this.endpointCount = integers.get(1);
+        this.requestDescriptionCount = integers.get(2);
+        this.cacheServerCount = integers.get(3);
+        this.cacheServerCapacity = integers.get(4);
     }
 
     private List<Integer> parseLineDetails(String line) {
@@ -37,8 +68,7 @@ public class InputParser {
         }
         return numberList;
     }
-    // line 1: number of videos, number of endpoints, number of request descriptions, number of cache servers, cache server capacity (MB)
-    // line 2: size of each video in MB
+
     // endpoint descriptions
     //  - line 1: latency from dc to endpoint
     //  - line 2: (K) number of cache servers connected to endpoint
