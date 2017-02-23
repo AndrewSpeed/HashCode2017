87431300e90fdd3a481df91c1367dde979ceecb1
Kris Watson <kris.watson@nanthealth.com>
2017-02-23 18:53:50 +0000

    Video and Cache Server

---
+++ src/main/java/CacheServer.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class CacheServer {
+	private int id;
+	private int capacity;
+	private List<Video> videos;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public List<Video> getVideos() {
+		return videos;
+	}
+	public void setVideos(List<Video> videos) {
+		this.videos = videos;
+	}
+}
