b2d0f8cb7abcd5baedd92a033c0b31a17ee30543
Kris Watson <kris.watson@nanthealth.com>
2017-02-23 21:04:27 +0000

    Results

---
+++ src/main/java/models/Results.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.List;
+
+public class Results {
+	private List<CacheServer> cacheServers;
+
+	public List<CacheServer> getCacheServers() {
+		return cacheServers;
+	}
+
+	public void setCacheServers(List<CacheServer> cacheServers) {
+		this.cacheServers = cacheServers;
+	}
+	
+}
